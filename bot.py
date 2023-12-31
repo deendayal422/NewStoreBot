8d7fb3404820cf89e4ac20cfc9fcd0631a8538f3
Sumit⁠˙⁠❥⁠˙सरकार <145983561+Sumit720304@users.noreply.github.com>
2023-12-31 10:59:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -111,23 +111,7 @@ async def Lazy_start():
         usr_cmd = cmd.text.split("_", 1)[-1]
         if usr_cmd == "/start":
             await add_user_to_database(bot, cmd)
-            if(Config.LAZY_MODE == True):
-                await cmd.reply_photo(photo=lazy_pic,
-                caption=Config.LAZY_HOME_TEXT.format(cmd.from_user.first_name, cmd.from_user.id),
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton("🍿supp⊕r† gr⊕up", url="https://t.me/LazyDeveloperSupport"),
-                            InlineKeyboardButton("🔊ß⊕†s chαηηεl", url="https://t.me/LazyDeveloper")
-                        ],
-                        [
-                            InlineKeyboardButton("🤖Aß⊕ut ß⊕†", callback_data="aboutbot"),
-                            InlineKeyboardButton("♥️Aß⊕ut Đ€V", callback_data="aboutdevs")
-                        ],
-                        [
-                            InlineKeyboardButton("⎝⎝✧✧ ᴡᴀᴛᴄʜ ᴛᴜᴛᴏʀɪᴀʟ ✧✧⎠⎠", url="https://youtu.be/Rtjyz3lEZwE")
-                        ]]))
-            else :
+            if :
                 await cmd.reply_text(
                 Config.HOME_TEXT.format(cmd.from_user.first_name, cmd.from_user.id),
                 disable_web_page_preview=True,
